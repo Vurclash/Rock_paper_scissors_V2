7c186582381909e3a6f5a844d7f220023f6d2f0a
Michael Brown <Vurclash@gmail.com>
2019-05-30 17:26:01 -0600

    added selections, attempted to activate user selection, could not define

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
 $(document).ready(function(){
-  
+  $("#play").on("click", function(){
+    var userSelect = $(this).attr["id"]
+    var compRPS = $(".selct")
+    var randomRPS = compRPS[Math.floor(Math.random() * compRPS.length)];
+    var compChoice = randomRPS.text
+    
+    
+    switch(userSelect){
+      case "Rock":
+        compChoice == "Scissors"
+        alert("You win")
+        break;
+      case "Rock":
+        compChoice == "Paper"
+        alert("You lost")
+        break;
+      case "Rock": 
+        compChoice == "Rock"
+        alert("Tie")
+        break;
+      default:
+        alert("Error: Test failed")
+      
+        
+    }
+    debugger
 
 
-
-
-
+    
+  });
 
 });
